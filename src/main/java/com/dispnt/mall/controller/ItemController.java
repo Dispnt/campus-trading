247c01078c3bdf0d1762128719f22bf65d9d8135
Dispnt <yuyuanjiajian@gmail.com>
2021-03-09 08:58:58 +0800

    add item with /item/add

--- src/main/java/com/dispnt/mall/controller/ItemController.java
+++ src/main/java/com/dispnt/mall/controller/ItemController.java
@@ -1,6 +1,8 @@
 package com.dispnt.mall.controller;
 
+import com.dispnt.mall.model.User;
 import com.dispnt.mall.repository.ItemRepository;
+import com.dispnt.mall.repository.UserRepository;
 import com.dispnt.mall.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
@@ -16,10 +18,16 @@ public class ItemController {
     @Autowired
     private ItemRepository itemRepository;
 
-    @PostMapping("")
-    public Item addItem(@RequestBody Item item){
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/add")
+    public Item addItem(@RequestHeader (value="Authorization") String jwt,@RequestBody Item item){
+        User user2 = userRepository.findByJsonWebToken(jwt);
+        item.setSellerId(user2.getId());
         return itemRepository.save(item);
     }
+    // {"name":"test","price":"123","imgUrl":"https://...","productComment":"苹果"}
 
     @GetMapping("/list")
     public List<Item> list(Model model){
