408961f0125889129655916edcab73dea8706143
dispnt <yuyuanjiajian@gmail.com>
2021-03-28 14:28:38 +0800

    spring security

--- src/main/java/com/dispnt/mall/SpringWebserviceApplication.java
+++ src/main/java/com/dispnt/mall/SpringWebserviceApplication.java
@@ -2,6 +2,14 @@ package com.dispnt.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class SpringWebserviceApplication {
@@ -9,4 +17,7 @@ public class SpringWebserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringWebserviceApplication.class, args);
     }
+
+
 }
+
