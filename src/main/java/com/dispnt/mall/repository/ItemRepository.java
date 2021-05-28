0c6de41d4309ac6426dfcfbc0da1f58b9cf83a61
dispnt <yuyuanjiajian@gmail.com>
2021-05-28 14:25:05 +0800

    fuzzy search

--- src/main/java/com/dispnt/mall/repository/ItemRepository.java
+++ src/main/java/com/dispnt/mall/repository/ItemRepository.java
@@ -3,5 +3,14 @@ package com.dispnt.mall.repository;
 import com.dispnt.mall.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Integer> {
+
+
+    List<Item> findByNameContaining(String searchContent);
+
+    Item findById(int itemId);
+
+
 }
--- src/main/java/com/dispnt/mall/repository/PurchaseHistoryRepository.java
+++ src/main/java/com/dispnt/mall/repository/PurchaseHistoryRepository.java
@@ -1,8 +1,13 @@
 package com.dispnt.mall.repository;
 
+import com.dispnt.mall.model.Item;
 import com.dispnt.mall.model.Purchase_history;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PurchaseHistoryRepository extends JpaRepository<Purchase_history, Integer>{
+import java.util.List;
 
+public interface PurchaseHistoryRepository extends JpaRepository<Purchase_history, Integer>{
+    List<Purchase_history> findByItemIdAndUserId(int itemId, int userId);
+
+//    void deleteById(int id);
 }
